60f7516522c43107abe1ec9438bdabe1a0dbf6c4
Aleksey Zgurskiy <a.zgurskiy@nullgr.com>
2018-01-15 15:40:14 +0200

    Initial commit

---
+++ DataRaft/DataRaft/DataRaft.h
@@ -0,0 +1,12 @@
+//
+//  DataRaft.h
+//  DataRaft
+//
+//  Created by Aleksey Zgurskiy on 15.01.2018.
+//  Copyright © 2018 Graviti Mobail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double DataRaftVersionNumber;
+FOUNDATION_EXPORT const unsigned char DataRaftVersionString[];
---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Aleksey Zgurskiy on 15.01.2018.
+//  Copyright © 2018 Graviti Mobail. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.rootViewController?.view.backgroundColor = UIColor.white
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
