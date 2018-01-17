822a702d81f1501bd6c45bbcc01d55116120455e
Aleksey Zgurskiy <a.zgurskiy@nullgr.com>
2018-01-17 12:44:50 +0200

    Added configuration method

---
+++ DataRaft/DataRaft/DataRaft/StoreType.swift
@@ -0,0 +1,26 @@
+//
+//  StoreType.swift
+//  DataRaft
+//
+//  Created by Aleksey Zgurskiy on 15.01.2018.
+//  Copyright © 2018 Graviti Mobail. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public enum StoreType {
+    public typealias RawValue = String
+    
+    case inMemory
+    case sqLite
+    
+    public var rawValue: RawValue {
+        switch self {
+        case .inMemory:
+            return NSInMemoryStoreType
+        default:
+            return NSSQLiteStoreType
+        }
+    }
+}
