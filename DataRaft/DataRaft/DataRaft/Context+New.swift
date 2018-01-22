d422aa180b4960ade05afab6372fcc2d70bff7d4
Aleksey Zgurskiy <a.zgurskiy@nullgr.com>
2018-01-22 17:30:08 +0200

    Helper for create new object

---
+++ DataRaft/DataRaft/DataRaft/Context+New.swift
@@ -0,0 +1,19 @@
+//
+//  Context+New.swift
+//  DataRaft
+//
+//  Created by Aleksey Zgurskiy on 22.01.2018.
+//  Copyright © 2018 Graviti Mobail. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension NSManagedObjectContext {
+    /// Creates a managed object and inserts it into that context.
+    ///
+    /// - Returns: The instance of object of the given type.
+    public func new<T: NSManagedObject>() -> T {
+        return NSEntityDescription.insertNewObject(forEntityName: T.entityName, into: self) as! T
+    }
+}
---
+++ DataRaft/DataRaftTests/DataRaft/Context+NewTests.swift
@@ -0,0 +1,33 @@
+//
+//  Context+NewTests.swift
+//  DataRaftTests
+//
+//  Created by Aleksey Zgurskiy on 22.01.2018.
+//  Copyright © 2018 Graviti Mobail. All rights reserved.
+//
+
+import XCTest
+@testable import DataRaft
+
+class Context_NewTests: XCTestCase {
+    let bundle = Bundle(for: Context_SaveTests.self)
+    var db: DataRaft!
+    
+    override func setUp() {
+        super.setUp()
+        db = DataRaft()
+        try! db.configure(modelName: "Model", bundle: bundle)
+    }
+    
+    override func tearDown() {
+        db = nil
+        super.tearDown()
+    }
+    
+    func testNewObject() {
+        db.performAndWaitOnMain { context in
+            let contact: Contact = context.new()
+            XCTAssertNotNil(contact)
+        }
+    }
+}
