8094af7d9c706e119351e4b2d4a3f1e54705edf5
Oleksii Zghurskyi <mr.noone@icloud.com>
2024-06-18 13:55:32 +0300

    Remove deprecated code

--- Sources/DataRaft/Deprecated/Extensions/SQLighter/SQLConvertible.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SQLighter
-
-public extension SQLConvertible {
-  func execute(on connection: Connection) throws {
-    let query = sqlQuery()
-    try connection.execute(sql: query.sql, args: [query.args])
-  }
-}
--- Sources/DataRaft/Deprecated/Protocols/TransactionObserver.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol TransactionObserver where Self: AnyObject {
-  func observes(event: ObserverEvent) -> Bool
-  func connectionDidChange(_ connection: Connection, event: ObserverEvent)
-  func connectionWillCommit(_ connection: Connection) throws
-  func connectionDidCommit(_ connection: Connection)
-  func connectionDidRollback(_ connection: Connection)
-}
